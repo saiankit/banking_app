8347c0f006bda869744b2b2430d0edd844cafe97
saiankit <saiankit30@gmail.com>
2020-07-20 20:53:33 +0530

    added main.dart

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:banking_app/home_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeWidget(),
+    );
+  }
+}
